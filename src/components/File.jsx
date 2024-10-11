2c28eb6eb4192a0583493ac4464b54232e1d4508
Loris <125302715+LorisCaruhel@users.noreply.github.com>
2024-10-11 21:17:10 +0200

    Update File.jsx

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -1,8 +1,14 @@
+/**
+ *
+ * @param {string} name
+ * @param {string} image
+ * @param {Object} props
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export function File({name, image, ...props}) {
-    return (
-        <div>
-            <img src={image} alt={name} />
-            <h3>{name}</h3>
-        </div>
-    )
+    return <div className="file">
+        <img src={image} alt={name}/>
+        <a href={image}>{name}</a>
+    </div>
 }
