ecb9c25b514f1e2094f67146d72da0b55c816116
Loris <125302715+LorisCaruhel@users.noreply.github.com>
2024-10-11 22:21:24 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import './App.css'
 import {File} from "./components/File.jsx";
 import {Folder} from "./components/Folder.jsx";
+import PDF from '/src/images/pdf.png';
+import CODE from '/src/images/code.png';
+import CSV from '/src/images/csv.png';
+import DOSSIER_OUVERT from '/src/images/dossier_ouvert.png';
+import DOSSIER_FERME from '/src/images/dossier_ferme.png';
+import FICHIER from '/src/images/fichier.png';
+import FLECHE from '/src/images/fleche_retour.png';
+import ZIP from '/src/images/zip.png';
 
 function App() {
   return <>
