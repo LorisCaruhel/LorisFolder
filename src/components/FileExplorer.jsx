094bacbdf4dfd192aba539a3ec790c148df4f4c8
LorisCaruhel <loriscaruhel3@gmail.com>
2024-10-12 18:17:58 +0200

    Ajout du support de plus d'extension vers icon

--- src/components/FileExplorer.jsx
+++ src/components/FileExplorer.jsx
@@ -5,14 +5,32 @@ import { Folder } from '/src/components/Folder.jsx';
 import { File } from '/src/components/File.jsx';
 
 import PDF from '/src/images/pdf.png';
-import CODE from '/src/images/code.png';
 import CSV from '/src/images/csv.png';
 import DOSSIER_OUVERT from '/src/images/dossier_ouvert.png';
 import DOSSIER_FERME from '/src/images/dossier_ferme.png';
 import FICHIER from '/src/images/fichier.png';
 import FLECHE from '/src/images/fleche_retour.png';
 import ZIP from '/src/images/zip.png';
-import {Button} from "./Button.jsx";
+import IMAGE from '/src/images/image.png';
+import DOCS from '/src/images/docs.png';
+import DIAPO from '/src/images/diaporama.png';
+import BINAIRE from '/src/images/binaire.png';
+import BASH from '/src/images/Bash-Dark.svg';
+import C from '/src/images/C.svg';
+import CSS from '/src/images/CSS.svg';
+import HTML from '/src/images/HTML.svg';
+import JAVA from '/src/images/Java-Dark.svg';
+import JS from '/src/images/JavaScript.svg';
+import JSON from '/src/images/json.png';
+import MD from '/src/images/Markdown-Dark.svg';
+import PHP from '/src/images/PHP-Dark.svg';
+import SQL from '/src/images/PostgreSQL-Dark.svg';
+import PYTHON from '/src/images/Python-Dark.svg';
+import REACT from '/src/images/React-Dark.svg';
+import SASS from '/src/images/Sass.svg';
+import TS from '/src/images/TypeScript.svg';
+
+import {Button} from "/src/components/Button.jsx";
 
 const FileExplorer = ({ owner, repo, branch }) => {
     const [currentPath, setCurrentPath] = useState(''); // Chemin courant
@@ -69,22 +87,57 @@ const FileExplorer = ({ owner, repo, branch }) => {
         const extension = fileName.split('.').pop().toLowerCase(); // Extraire l'extension
 
         switch (extension) {
+            case 'out':
+                return BINAIRE;
+            case 'pptx':
+            case 'ppt':
+                return DIAPO;
+            case 'png':
+            case 'jpg':
+            case 'jpeg':
+            case 'webp':
+            case 'svg':
+                return IMAGE;
             case 'pdf':
                 return PDF;
+            case 'xlsx':
             case 'csv':
                 return CSV;
+            case 'rar':
             case 'zip':
                 return ZIP;
+            case 'docx':
+            case 'odt':
+                return DOCS;
             case 'js':
+                return JS;
             case 'jsx':
+                return REACT;
             case 'html':
+                return HTML;
             case 'css':
+                return CSS;
             case 'json':
+                return JSON;
             case 'c':
+                return C;
             case 'php':
+                return PHP;
             case 'java':
+                return JAVA;
+            case 'sql':
+                return SQL;
             case 'md':
-                return CODE;
+                return MD;
+            case 'sh':
+            case 'bash':
+                return BASH;
+            case 'scss':
+                return SASS;
+            case 'ts':
+                return TS;
+            case 'py':
+                return PYTHON;
             default:
                 return FICHIER;
         }
