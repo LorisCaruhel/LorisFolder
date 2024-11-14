c943edcdba09282d3acd246379e61fe97dc3360b
LorisCaruhel <LorisCaruhel@users.noreply.github.com>
2024-11-14 13:48:38 +0000

    deploy: 7d5d5d9c425a868da08dee558dffeff9fe9f6296

--- Cours/BUT Info/S3/R3.07 - SQL dans un langage de programmation/TD/TD2/exo3_U1.sql
+++ Cours/BUT Info/S3/R3.07 - SQL dans un langage de programmation/TD/TD2/exo3_U1.sql
@@ -1,8 +1,8 @@
 START TRANSACTION ;
 
-SELECT points FROM enfant WHERE nopers = 1 FOR UPDATE ;
+SELECT points FROM enfant WHERE nopers = 1 FOR UPDATE ; -- RS
 
-UPDATE enfant SET points = points + 8 WHERE nopers = 1 ;
+UPDATE enfant SET points = points + 8 WHERE nopers = 1 ; -- RX
 
 COMMIT ;
 
@@ -16,4 +16,4 @@ COMMIT ;
 -- U2 t7 COMMIT ;
 -- U1 t8 COMMIT ;
 
--- Vrai odre des transactions : t1, t2, t3, t6(+8), t7, t4, t5(+5), t8
+-- Vrai odre des transactions : t1, t2, t3, t6(+8), t8, t4, t5(+5), t7
--- Cours/BUT Info/S3/R3.07 - SQL dans un langage de programmation/TD/TD2/exo4_U1.sql
+++ Cours/BUT Info/S3/R3.07 - SQL dans un langage de programmation/TD/TD2/exo4_U1.sql
@@ -1,7 +1,18 @@
 START TRANSACTION ;
 
-UPDATE enfant SET points = points + 5 WHERE nopers = 1 ;
+UPDATE enfant SET points = points + 5 WHERE nopers = 1 ; -- RX
 
-UPDATE enfant SET points = points + 9 WHERE nopers = 2 ;
+UPDATE enfant SET points = points + 9 WHERE nopers = 2 ; -- RX
 
 COMMIT ;
+
+
+-- U1 t1 START TRANSACTION ;
+-- U2 t2 START TRANSACTION ;
+-- U1 t3 UPDATE enfant SET points = points + 5 WHERE nopers = 1 ;
+-- U2 t4 UPDATE enfant SET points = points + 7 WHERE nopers = 2 ;
+-- U1 t5 UPDATE enfant SET points = points + 9 WHERE nopers = 2 ;
+-- U2 t6 UPDATE enfant SET points = points + 11 WHERE nopers = 1 ;
+-- U2 t7 COMMIT ;
+-- U1 t8 COMMIT ;
+-- Vrai odre des transactions : t1, t2, t3, t4, assassinat de U2, le relancer indépendament.
