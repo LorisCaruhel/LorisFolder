094c35f30ba04ed4462aff5088d6edf5fc46e230
lcaruhel <lcaruhel@pce-test>
2025-01-21 11:57:25 +0100

    Ajout de SQL R4.03

---
+++ Cours/BUT Info/S3/R3.05 - Programmation système/TP7/tp7.c
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/sem.h>
+
+int main() {
+    key_t cle;
+    cle = ftok(".", 0);
+
+    int handler;
+    handler = semget(cle, 1, IPC_CREAT | 0640);
+
+    int ret;
+    struct sembuf arr_sop[2];
+    arr_sop[0].sem_num = 0;
+    arr_sop[0].sem_op = 1; // On consomme une qté de 1 sur sémaphore 0
+    arr_sop[0].sem_flg = 0;
+    arr_sop[1].sem_num = 1;
+    arr_sop[1].sem_op = -2; // On consomme une qté de 2 sur sémaphore 1
+    arr_sop[1].sem_flg = 0;
+    ret = semop(handler, arr_sop, 2);
+
+    printf("%x\n", ret);
+}
